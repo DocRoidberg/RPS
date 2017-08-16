9dd4895ffc39c28b38dead43ef7b43fe9e1ca173
CodeScop <StefBWard@gmail.com>
2017-08-16 10:03:10 -0400

    got rps working

--- assets/hw.js
+++ assets/hw.js
@@ -1,29 +1,46 @@
+var choice = "";
+function getRand(min, max){
+	min = Math.ceil(min);
+	max = Math.floor (max);
+	var num = Math.floor(Math.random() * (max-min)) + min; 
+		if(num === 1){
+			return "rock";
+		}else if (num === 2){
+			return "paper";
+		}else {
+			return "scissors";
+		}
+}
+
+function playRound(userC, choice) {
+	if (userC === choice) {
+		alert("You and the computer have both selected " + choice + ". You tie!");
+	} else if(userC === "rock" && choice === "scissors"){
+		alert("You chose rock and the computer chose scissors. You win!");
+	} else if(userC === "paper" && choice === "rock"){
+		alert("You chose paper and the computer chose rock. You win!");
+	} else if(userC === "scissors" && choice === "paper"){
+		alert("You chose scissors and the computer chose paper. You win!");
+	} else {
+		alert("You chose " + userC + ", but the computer chose " + choice + ". You lose.");
+	}
+}
+
 $('#rock').click(function(){
-	userC = "rock";
-	alert("You have selected rock");
+	choice = getRand(1,4);
+	playRound("rock", choice);
 	//how to how computer check and compare function?
 });
 
 $('#paper').click(function(){
-	alert("You have selected paper");
-	userc = "paper";
+	choice = getRand(1,4);
+	playRound("paper", choice);
 });
 
 $('#scissors').click(function(){
-	alert("You have selected scissors");
-	userC = "scissors";
+	choice = getRand(1,4);
+	playRound("scissors", choice);
 });
 
-function getRand(min, max){
-	min = Math.ceil(min);
-	max = Math.floor (max);
-	num = return Math.floor(Math.random() * (max-min)) + min; 
-		if(num = 1){
-			choice = "rock"
-		}else if (num = 2){
-			choice = "paper"
-		}else {
-			choice = "scissors"
-		}
-}	
-console.log(choice)	
+
+
